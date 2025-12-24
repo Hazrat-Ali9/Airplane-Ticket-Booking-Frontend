e880bd79789a496fbd09861ab5d0556c05348a2a
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-24 11:30:07 +0600

    verify token  :rocket:

--- src/admin/VerifyTicket.jsx
+++ src/admin/VerifyTicket.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import QRScan from "./qr-reader/qr-reader.component";
 import QRScanner from "./qr-reader/QRScanner";
-
+// verify token
 const VerifyTicket = () => {
   return (
     <div className="px-[30px] md:px-[30px]">
